023e131c42c59cb4d72e079c425a4f6b5244d5cb
Lucioschenkel <lucio.schenkel@proton.me>
2026-03-08 07:11:04 -0300

    feat: add go to references keymap

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,13 +8,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
         client.server_capabilities.semanticTokensProvider = nil
       end
     end
-    
+
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = args.buf,
       callback = function()
         vim.lsp.buf.format({ async = false, id = args.data.client_id })
       end,
     })
+
+    -- set keymap for 'go to references' using Telescope
+    vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references,
+      { buffer = args.buf, desc = "Go to references" })
   end
 })
-
--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,1 +1,2 @@
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode" })
